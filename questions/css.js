15efc35d7b22694baaf6b7b2b360707b5a0a7d8d
OcelotDive <oscar1dlh@hotmail.com>
2018-06-29 06:02:35 +0100

    Update css.js
    Changed phrasing of several questions

--- questions/css.js
+++ questions/css.js
@@ -1,10 +1,7 @@
 'use strict';
 
 module.exports = {
-    /**
-     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
-     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
-     */
+
     QUESTIONS_EN_GB: [
         {
             'Which properties do inline elements not possess under normal document flow?': [
