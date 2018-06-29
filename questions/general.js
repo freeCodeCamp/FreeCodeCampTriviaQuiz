4b2c8fbd506baf9a1695420bd86fba83cfc317dc
OcelotDive <oscar1dlh@hotmail.com>
2018-06-29 06:01:20 +0100

    Update general.js

--- questions/general.js
+++ questions/general.js
@@ -1,10 +1,7 @@
 'use strict';
 
 module.exports = {
-    /**
-     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
-     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
-     */
+   
     QUESTIONS_EN_GB: [
          {
             'Which of the following is not a common sorting algorithm?': [
