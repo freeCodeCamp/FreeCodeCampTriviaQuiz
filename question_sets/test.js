6c024e371fd920a04900192bd9c85abe61f2032a
ocelotdive <oscar1dlh@hotmail.com>
2019-12-13 09:13:40 +0000

    add: question set folder- removed duplicate question sets

---
+++ question_sets/test.js
@@ -0,0 +1,195 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    QUESTIONS_EN_GB: [
+        {
+        'A  C s s selector using square bracket notation targets, what?': [
+                'An element with a specific attribute.',
+                'An element with inner Html.',
+                'An empty element.',
+                'An element with an image.',
+               
+            ],
+        },
+        {
+            'The Zed-index property will not work on elements with which position?': [
+                'Static.',
+                'Fixed.',
+                'Absolute.',
+                'Relative',
+                'Sticky',
+            ],
+        },
+        {
+            'Which property would be used to add space between an elements border and inner content?': [
+                'Padding',
+                'Outline',
+                'Margin',
+                'Indent',
+            ],
+        },
+        {
+            'Which property would alter the size of an elements border?': [
+                'Border width',
+                'Border size',
+                'Border height',
+                'Border padding',
+            ],
+        },
+
+        {
+            'How would you start a comment in a C s s stylesheet?': [
+                'Forward slash asterisk.',
+                'Forward slash , forward slash.',
+                'Between angle brackets',
+                'Using hash or pound symbol',
+            ],
+        },
+        {
+            'What will be the total space between two adjacently stacked block level elements if, the top element has a bottom margin of thirty pixels, and the lower element has a top margin of twenty pixels?': [
+                'Thirty pixels.',
+                'Twenty pixels.',
+                'Fifty pixels.',
+                'Forty pixels.',
+              
+            ],
+        },
+        {
+            'Which of the following is not a valid C s s border style?': [
+                'Gradient',
+                'Solid',
+                'Dashed',
+                'Outset',
+                'None',
+                'Hidden',
+              
+            ],
+        },
+        {
+            'Which of the following is not a valid C s s unit of measurement?': [
+                'Factor',
+                'Millimeters',
+                'Pica',
+                'Pixels',
+                'Centimeters', 
+            ],
+        },
+        {
+            'Type, Class and ID, deal with which concept in C s s?': [
+                'Specificity',
+                'Box Model',
+                'Positioning',
+                'Cascade',
+                'Flex', 
+            ],
+        },
+        {
+            'Which rule would you use to define a media query in a stylesheet?': [
+                '@ media',
+                '# query',
+                'Universal',
+                '# media',
+                '@ media query', 
+            ],
+        },
+    ],
+    
+    QUESTIONS_EN_US: [
+        {
+        'A  C s s selector using square bracket notation targets, what?': [
+                'An element with a specific attribute.',
+                'An element with inner Html.',
+                'An empty element.',
+                'An element with an image.',
+               
+            ],
+        },
+        {
+            'The Zed-index property will not work on elements with which position?': [
+                'Static.',
+                'Fixed.',
+                'Absolute.',
+                'Relative',
+                'Sticky',
+            ],
+        },
+        {
+            'Which property would be used to add space between an elements border and inner content?': [
+                'Padding',
+                'Outline',
+                'Margin',
+                'Indent',
+            ],
+        },
+        {
+            'Which property would alter the size of an elements border?': [
+                'Border width',
+                'Border size',
+                'Border height',
+                'Border padding',
+            ],
+        },
+
+        {
+            'How would you start a comment in a C s s stylesheet?': [
+                'Forward slash asterisk.',
+                'Forward slash , forward slash.',
+                'Between angle brackets',
+                'Using hash or pound symbol',
+            ],
+        },
+        {
+            'What will be the total space between two adjacently stacked block level elements if, the top element has a bottom margin of thirty pixels, and the lower element has a top margin of twenty pixels?': [
+                'Thirty pixels.',
+                'Twenty pixels.',
+                'Fifty pixels.',
+                'Forty pixels.',
+              
+            ],
+        },
+        {
+            'Which of the following is not a valid C s s border style?': [
+                'Gradient',
+                'Solid',
+                'Dashed',
+                'Outset',
+                'None',
+                'Hidden',
+              
+            ],
+        },
+        {
+            'Which of the following is not a valid C s s unit of measurement?': [
+                'Factor',
+                'Millimeters',
+                'Pica',
+                'Pixels',
+                'Centimeters', 
+            ],
+        },
+        {
+            'Type, Class and ID, deal with which concept in C s s?': [
+                'Specificity',
+                'Box Model',
+                'Positioning',
+                'Cascade',
+                'Flex', 
+            ],
+        },
+        {
+            'Which rule would you use to define a media query in a stylesheet?': [
+                '@ media',
+                '# query',
+                'Universal',
+                '# media',
+                '@ media query', 
+            ],
+        },
+    ]
+   
+    
+};
